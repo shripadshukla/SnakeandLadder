5d0c9ef07cbdb69dbc62e979614c7533fc90fdda
shripadshukla <shripadshukla46@gmail.com>
2021-05-01 01:47:55 +0530

    UC3: Player checks for opton added successfully

--- snakeandladder/SnakeAndLadder.java
+++ snakeandladder/SnakeAndLadder.java
@@ -4,11 +4,66 @@ public class SnakeAndLadder {
 
 	public static void main(String []args) {
 
-		double dice = Math.floor(Math.random() * 10) % 7;
-
-		System.out.println("Player roll a dice");
-
-		System.out.println("Dice number is : " + dice);
+		int dice = (int) Math.floor(Math.random() * 10) % 7;
+		
+		int dicepos=0;
+		
+		if (dice == 0)
+			System.out.println("No movements");
+		
+		switch (dice) {
+		
+		case 1:
+			System.out.println("Ladder");
+			dicepos = dicepos + dice;
+			System.out.println(dicepos);
+			
+			break;
+			
+		case 2:
+			System.out.println("Ladder");
+			dicepos = dicepos + dice;
+			System.out.println(dicepos);
+			
+			break;
+			
+		case 3:
+			System.out.println("Ladder");
+			dicepos = dicepos + dice;
+			System.out.println(dicepos);
+		    
+			break;
+		    
+		case 4:
+			System.out.println("Snake");
+			dicepos = dicepos - dice;
+			System.out.println(dicepos);
+		
+			break;
+			
+		case 5:
+			System.out.println("Snake");
+			dicepos = dicepos - dice;
+			System.out.println(dicepos);
+			
+			break;
+			
+		case 6:
+			System.out.println("Snake");
+			dicepos = dicepos - dice;
+			System.out.println(dicepos);
+			
+			break;
+			
+		default:
+			System.out.println("No Moves");
+			dicepos = dicepos;
+			System.out.println(dicepos);
+			
+			break;
+		}
+		
+		dicepos = dicepos+dice;
 	}
 
 }
